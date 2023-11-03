b8d892fbd5110138568a50197b9fdc8310414471
GonzaloBounzerio <bounzeriog@gmail.com>
2023-11-03 18:55:56 -0300

    cambios2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+//Al hacer click en btnNavbar que tenga en cuenta el alto de la NavBar para que no tape los divs apuntados.
+window.addEventListener("hashchange", function() { scrollBy(0, -100) })
 
 class dataMosaico {
     constructor(id,title,image,imageHover){
@@ -11,9 +13,9 @@ class dataMosaico {
 const item1 = new dataMosaico(1,"Ventanas de aluminio","assets/img/ventanas1.jpg","assets/img/ventanas2.jpg")
 const item2 = new dataMosaico(2,"Puertas de aluminio","assets/img/puertas1.jpg","assets/img/puertas2.jpg")
 const item3 = new dataMosaico(3,"Tecnología","assets/img/tecnologia1.jpg","assets/img/tecnologia2.jpg")
-const item4 = new dataMosaico(4,"Puertas plegables PVC","assets/img/plegables1.jpg","assets/img/plegables2.jpg")
-const item5 = new dataMosaico(5,"Seguridad","assets/img/seguridad1.jpg","assets/img/seguridad2.png")
-const item6 = new dataMosaico(6,"Aberturas de madera","assets/img/maderas1.jpg","assets/img/maderas2.jpg")
+const item4 = new dataMosaico(4,"Seguridad","assets/img/seguridad1.jpg","assets/img/seguridad2.png")
+const item5 = new dataMosaico(5,"Aberturas de madera","assets/img/maderas1.jpg","assets/img/maderas2.jpg")
+const item6 = new dataMosaico(6,"Y muchos productos más...","assets/img/plegables1.jpg","assets/img/plegables2.jpg")
 
 let arrayMosaicos=[]
 
