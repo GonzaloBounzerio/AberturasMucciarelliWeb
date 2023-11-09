517cff7ac78f4cd350a816adeb278d1d4a68152d
GonzaloBounzerio <bounzeriog@gmail.com>
2023-11-08 21:16:04 -0300

    historia catalogo animaciones

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ class dataMosaico {
 
 const item1 = new dataMosaico(1,"Ventanas de aluminio","assets/img/ventanas1.jpg","assets/img/ventanas2.jpg")
 const item2 = new dataMosaico(2,"Puertas de aluminio","assets/img/puertas1.jpg","assets/img/puertas2.jpg")
-const item3 = new dataMosaico(3,"Tecnología","assets/img/tecnologia1.jpg","assets/img/tecnologia2.jpg")
+const item3 = new dataMosaico(3,"Redes de protección","assets/img/redes1.jpg","assets/img/redes2.jpg")
 const item4 = new dataMosaico(4,"Seguridad","assets/img/seguridad1.jpg","assets/img/seguridad2.png")
 const item5 = new dataMosaico(5,"Aberturas de madera","assets/img/maderas1.jpg","assets/img/maderas2.jpg")
 const item6 = new dataMosaico(6,"Y mucho más...","assets/img/plegables1.jpg","assets/img/plegables2.jpg")
@@ -82,10 +82,6 @@ let btnConsultas = document.getElementById("btnNav3")
 let popUp = document.getElementsByClassName("containerPopUp")[0]
 let btnCierraPopUp = document.getElementById("btnCierraPopUp")
 btnConsultas.onclick = () => {
-    /*window.scroll({
-        top: 0,
-        behavior: "smooth",
-      });*/
     popUp.style.visibility=("visible")
 }
 btnCierraPopUp.onclick = () => {
